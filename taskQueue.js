02f164b7c7b5baf766344c25ee4d39bab4014398
girijakangutkar <girijakangutkar2002@gmail.com>
2024-09-22 17:19:07 +0530

    Update taskQueue.js

--- taskQueue.js
+++ taskQueue.js
@@ -1,4 +1,5 @@
 const Queue = require("bull");
+const winston = require("winston");
 
 const taskQueue = new Queue("taskQueue", {
   redis: {
@@ -10,9 +11,22 @@ const taskQueue = new Queue("taskQueue", {
 taskQueue.process(async (job) => {
   try {
     console.log(`Processing job: ${JSON.stringify(job.data)}`);
-    const { user_id, task } = job.data;
-    const taskFunction = eval(`(${task})`);
-    const result = await taskFunction(user_id);
+    const { user_id, taskFunction, loggerOptions } = job.data;
+
+    // Recreate the logger
+    const logger = winston.createLogger({
+      level: loggerOptions.level,
+      format: winston.format[loggerOptions.format](),
+      transports: [
+        new winston.transports.File({ filename: loggerOptions.filename }),
+      ],
+    });
+
+    // Create the task function
+    const createTaskFn = eval(`(${taskFunction})`);
+    const task = createTaskFn(logger);
+
+    const result = await task(user_id);
     console.log(`Completed job for user_id: ${user_id}, result: ${result}`);
     return result;
   } catch (err) {
